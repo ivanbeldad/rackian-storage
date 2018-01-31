4e123daf3746dca7434e20066418f12ade5cfa48
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-01-31 23:37:24 +0100

    refactor(core): change config format, remove hello world tests

--- src/helloWorld.spec.js
+++
@@ -1,8 +0,0 @@
-require('jest')
-const helloWorld = require('./helloWorld')
-
-describe('Temporal test', () => {
-  it('Hello World should return Hello World', () => {
-    expect(helloWorld).toBe('Hello World!')
-  })
-})
