2bd6ede8b245f5c96c6b3fda5e9c9da1f5c0dc7e
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-02-17 21:56:02 +0100

    refactor(core): split if then in two lines

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,7 +8,8 @@ mkdir $BIN_DIR
 export PATH=$BIN_DIR:$PATH
 
 # If the SDK is not already cached, download it and unpack it
-if [ ! -d ${HOME}/google-cloud-sdk/bin ]; then
+if [ ! -d ${HOME}/google-cloud-sdk/bin ]
+then
   rm -rf ${HOME}/google-cloud-sdk
   export CLOUDSDK_CORE_DISABLE_PROMPTS=1
   curl https://sdk.cloud.google.com | bash;
