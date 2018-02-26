6f94f3c7db8590d1de0e6bde097696501c1b838c
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-02-26 22:59:32 +0100

    test(folders): add constructor test and improve validation

--- src/folders/Folder.spec.js
+++ src/folders/Folder.spec.js
@@ -5,6 +5,7 @@ const { validationResult } = require('express-validator/check')
 const Folder = require('./Folder')
 
 let req = httpMocks.createRequest()
+let next = jest.fn()
 
 beforeEach(() => {
   req.body = {
@@ -12,10 +13,40 @@ beforeEach(() => {
 })
 
 describe('Folder', () => {
-  it('Should throw error if name does not exists', () => {
-    const validate = () => {
+  it('Should not throw error on empty constructor or with an object', () => {
+    expect(() => new Folder()).not.toThrowError()
+    expect(() => new Folder({})).not.toThrowError()
+  })
+
+  it('Should assign properties correctly', () => {
+    const folder = new Folder({
+      url: 'url',
+      id: 'id',
+      name: 'name',
+      parentFolder: 'parentFolder',
+      files: 'files',
+      links: 'links'
+    })
+    expect(folder.url).toBe('url')
+    expect(folder.id).toBe('id')
+    expect(folder.name).toBe('name')
+    expect(folder.parentFolder).toBe('parentFolder')
+    expect(folder.files).toBe('files')
+    expect(folder.links).toBe('links')
+  })
+
+  it('Should not validate if name does not exists', () => {
+    const done = []
+
+    Folder.validation().forEach(async fn => {
+      done.push(new Promise(resolve => {
+        fn(req, null, resolve)
+      }))
+    })
+
+    Promise.all(done).then(() => {
       expect(validationResult(req).isEmpty()).toBeFalsy()
-    }
-    Folder.validation()[0](req, null, validate)
+      expect(validationResult(req).mapped()).toHaveProperty('name')
+    })
   })
 })
