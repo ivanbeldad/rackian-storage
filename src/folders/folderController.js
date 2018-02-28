f064f80582881c727476482c73a6ef5c06b6b07f
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-02-28 23:59:13 +0100

    feat(folders): add basic folder controller structure

---
+++ src/folders/folderController.js
@@ -0,0 +1,24 @@
+// const Folder = require('./Folder')
+// const { validationResult } = require('express-validator/check')
+
+const get = (req, res, next) => {
+
+}
+
+const getById = (req, res, next) => {
+
+}
+
+const post = (req, res, next) => {
+
+}
+
+const put = (req, res, next) => {
+
+}
+
+const del = (req, res, next) => {
+
+}
+
+module.exports = { get, getById, post, put, del }
---
+++ src/folders/folderController.spec.js
@@ -0,0 +1,15 @@
+require('jest')
+
+// const httpMocks = require('node-mocks-http')
+// const folderController = require('./folderController')
+
+// let req = httpMocks.createRequest()
+// let res = httpMocks.createResponse()
+// res.status = jest.fn(() => res)
+// res.send = jest.fn(() => res)
+// res.end = jest.fn(() => res)
+
+describe('Folder Controller', () => {
+  it('', () => {
+  })
+})
