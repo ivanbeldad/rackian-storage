0197d5e724e80ea87950e1f4a3832d7869dbb53a
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-02-28 23:59:36 +0100

    feat(core): add folder routes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,15 @@
 const router = require('express').Router()
+const authMiddleware = require('./middlewares/authMiddleware')
 const healthController = require('./health/healthController')
+const folderController = require('./folders/folderController')
+const Folder = require('./folders/Folder')
 
-router.use('/healthz', healthController.get)
+router.get('/healthz', healthController.get)
+
+router.get(Folder.uri, authMiddleware, folderController.get)
+router.get(`${Folder.uri}/:id`, authMiddleware, folderController.getById)
+router.post(Folder.uri, authMiddleware, Folder.validation(), folderController.post)
+router.put(`${Folder.uri}/:id`, Folder.validation(), authMiddleware, folderController.put)
+router.delete(`${Folder.uri}/:id`, authMiddleware, folderController.del)
 
 module.exports = router
