668877831045efbbbd1232887e810c2f9b2932ab
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-04-30 09:16:04 +0200

    fix: add path to every route

--- src/router.js
+++ src/router.js
@@ -7,14 +7,14 @@ const folderController = require('./folders/folderController')
 const Folder = require('./folders/Folder')
 const folderRouter = express.Router()
 
-healthRouter.get(healthController.get)
+healthRouter.get('/', healthController.get)
 
-folderRouter.use(authMiddleware)
-folderRouter.get(folderController.get)
-folderRouter.get('/:id', folderController.getById)
-folderRouter.post(Folder.validation(), folderController.post)
-folderRouter.put('/:id', Folder.validation(), folderController.put)
-folderRouter.delete('/:id', folderController.del)
+folderRouter.use('/', authMiddleware)
+folderRouter.get('/', folderController.get)
+folderRouter.get('/:id/', folderController.getById)
+folderRouter.post('/', Folder.validation(), folderController.post)
+folderRouter.put('/:id/', Folder.validation(), folderController.put)
+folderRouter.delete('/:id/', folderController.del)
 
 module.exports = {
   enroute: (app) => {
